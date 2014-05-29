3064ce5778cba592eabd3171272e3fb0d627acc4
Andy Smith <andjsmit@iupui.edu>
2014-05-29 14:41:15 -0400

    Added background color, fonts, and some style changes.

--- all/themes/hs_church_zen/templates/html.tpl.php
+++ all/themes/hs_church_zen/templates/html.tpl.php
@@ -25,7 +25,7 @@
   <meta http-equiv="cleartype" content="on">
 
   <?php print $styles; ?>
-  <link href='http://fonts.googleapis.com/css?family=Didact+Gothic' rel='stylesheet' type='text/css'>
+  
   <?php print $scripts; ?>
   <?php if ($add_html5_shim and !$add_respond_js): ?>
     <!--[if lt IE 9]>
